47a187411c08917bf94dd754d4f899f18ee2e572
Matthew Bond <matthew.randal.bond@gmail.com>
2017-12-06 15:57:58 -0800

    Changed import to no longer reference the deprecated ws package

--- server/handlers/game.go
+++ server/handlers/game.go
@@ -7,19 +7,18 @@ import (
 	"net/http"
 
 	"github.com/info344-a17/typy-bird/server/models"
-	"github.com/info344-a17/typy-bird/server/ws"
 	"gopkg.in/mgo.v2/bson"
 )
 
 //HandlerContext keeps track of database information
 type HandlerContext struct {
-	Notifier   *ws.Notifier
+	Notifier   *Notifier
 	GameRoom   *models.GameRoom
 	TypieStore *models.MongoStore
 }
 
 //NewHandlerContext creates a new instance of a context struct to be used by a handler
-func NewHandlerContext(notifier *ws.Notifier, gameRoom *models.GameRoom, typieStore *models.MongoStore) *HandlerContext {
+func NewHandlerContext(notifier *Notifier, gameRoom *models.GameRoom, typieStore *models.MongoStore) *HandlerContext {
 	return &HandlerContext{
 		Notifier:   notifier,
 		GameRoom:   gameRoom,
