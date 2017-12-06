e4ec6d9021c432e8aa1908dcfc2ebc3d3e278dc8
Eric Jacobson <eric.jj96@gmail.com>
2017-12-06 12:12:34 -0800

    remove comments

--- server/models/mongostore.go
+++ server/models/mongostore.go
@@ -34,7 +34,6 @@ func NewMongoStore(sess *mgo.Session, dbName string, collectionName string) *Mon
 func (s *MongoStore) GetByID(id bson.ObjectId) (*TypieBird, error) {
 	typieBird := &TypieBird{}
 	col := s.session.DB(s.dbname).C(s.colname)
-	fmt.Println(id)
 	err := col.FindId(id).One(&typieBird)
 	if err != nil {
 		return nil, ErrTypieBirdNotFound
@@ -56,8 +55,6 @@ func (s *MongoStore) GetByUserName(username string) (*TypieBird, error) {
 //InsertTypieBird inserts a new typie bird into the mongo store and returns the typie bird
 func (s *MongoStore) InsertTypieBird(newTypie *TypieBird) (*TypieBird, error) {
 	col := s.session.DB(s.dbname).C(s.colname)
-	// newTypie.ID = bson.NewObjectId()
-	fmt.Println(newTypie.ID)
 	if err := col.Insert(newTypie); err != nil {
 		return nil, fmt.Errorf("error inserting typie bird: %v", err)
 	}
