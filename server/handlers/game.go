2f8c56a5738868b14ad5322ff8c4fa23b87a08d8
Matthew Bond <matthew.randal.bond@gmail.com>
2017-12-06 18:20:22 -0800

    Added dictionary get handler

--- server/handlers/game.go
+++ server/handlers/game.go
@@ -277,6 +277,28 @@ func (c *HandlerContext) ReadyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//LeaderboardHandler retrieves the top 10 in ascending order
+func (c *HandlerContext) LeaderboardHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		//get top scorers from mongo store
+		leaderboard, err := c.TypieStore.GetTopScores()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error marshalling leaderboard to JSON: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(leaderboard); err != nil {
+			http.Error(w, fmt.Sprintf("error encoding leaderboard to JSON: %v", err), http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 //DictHandler handles the /dictionary route and gets a list of random words that the users must type to complete the game
 func (c *HandlerContext) DictHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
