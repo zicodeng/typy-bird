5904a6db15625d03c304441073dd5bf161a6fe8f
Eric Jacobson <eric.jj96@gmail.com>
2017-12-06 19:56:47 -0800

    fix leaderboard

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,16 +9,38 @@ class Index extends React.Component<any, any> {
 		super(props, context);
 
 		this.state = {
-			gameRoom: null
+			disabled: false,
+			available: true,
+			leaderboard: []
 		};
 	}
 
+	private fetchTopScores= (): void => {
+		const url = `http://${this.getCurrentHost()}/leaderboard`;
+		axios
+			.get(url)
+			.then(res => {
+				this.setState({
+					leaderboard: res.data
+				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
 	public render() {
+		var button;
+		if (this.state.available) {
+			button = <button onClick={e => this.postTypie()} disabled={this.state.disabled}>PLAY</button>;
+		} else {
+			button = <button disabled={true} onClick={e => this.postTypie()} >PLAY</button>;
+		}
 		return (
 			<div>
 				<h1>Hello, New Typies</h1>
 				<input type="text" ref="username" id="username" />
-				<button onClick={e => this.postTypie()}>PLAY</button>
+				{button}
 				{this.renderTable()}
 			</div>
 		);
@@ -26,7 +48,9 @@ class Index extends React.Component<any, any> {
 
 	public componentWillMount() {
 		//establish websocket collection
+		this.fetchTopScores();
 		let host = this.getCurrentHost();
+
 		const websocket = new WebSocket('ws://' + host + '/ws');
 
 		websocket.addEventListener('error', function(err) {
@@ -39,34 +63,60 @@ class Index extends React.Component<any, any> {
 			console.log('Websocket connection closed');
 		});
 		websocket.addEventListener('message', event => {
-			const gameRoom = JSON.parse(event.data);
-			this.setState({
-				gameRoom: gameRoom
-			});
+			const topScores = JSON.parse(event.data);
+			console.log(topScores)
+			if (topScores.type == "Leaderboard")  {
+				console.log(topScores.payload.available)
+				this.setState({
+					available: topScores.payload.available,
+					leaderboard: topScores.payload.leaders
+				});
+			}
 		});
 	}
 
 	private renderTable = (): JSX.Element => {
 		const thead = (
+
 			<thead>
 				<tr>
+					<td>
+						<h3>
+							{this.state.available? "Gameroom: Available": "Gameroom: Unavailable"}
+						</h3>
+					</td>
+				</tr>
+				<tr>
+					<th>Rank</th>
 					<th>Username</th>
 					<th>Best Record</th>
 				</tr>
 			</thead>
 		);
-		const rows = (
-			{}
-		)
+		var count = 0;
+		var scores = this.state.leaderboard
+		.map((leader) => {
+			count++;
+			return (
+				<tr key={count}>
+					<td>{count}</td>
+					<td>{leader.userName}</td>
+					<td>{leader.record}</td>
+				</tr>
+			)
+		})
+
 		const tbody = (
 			<tbody>
-				{rows}
+					{scores}
 			</tbody>
 		)
-		return <table>{thead}</table>;
+		return <table>{thead}{tbody}</table>;
 	};
 
 	private postTypie = () => {
+		this.setState({ disabled: true});
+		console.log("fuck");
 		let username = this.refs.username['value'].trim();
 		let typie = {
 			userName: ''
